cb1014e83ad3b0af5ec26360bb03fed76173c394
bpuyol <148617619+bpuyol@users.noreply.github.com>
2024-03-16 11:12:13 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -4,29 +4,56 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('people.id'),nullable=True)
+    planets_id = Column(Integer, ForeignKey('planets.id'),nullable=True)
+    vehiculos_id = Column(Integer, ForeignKey('vehiculos.id'),nullable=True)
+    usuarios_id = Column(Integer, ForeignKey('usuarios.id'),nullable=False)
+
+
+class Usuarios(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(250))
+    pasajeros = Column(Integer, nullable=False)
+    usuarios_favoritos = relationship(Favoritos)
+    
+
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    people_favoritos = relationship(Favoritos)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    climate = Column(String(250))
+    population = Column(Integer, nullable=False)
+    planets_favoritos = relationship(Favoritos)
+    
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(250))
+    pasajeros = Column(Integer, nullable=False)
+    vehiculos_favoritos = relationship(Favoritos)
+    
+
 
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
